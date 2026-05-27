fe180d9ee40aa5ef20fb3ae82fc8926981953dd4
FelixJP23 <felixsh230517@gmail.com>
2026-05-27 09:05:08 -0300

    Template da Home

--- DetectiveLab_Site/DetectiveLab_Site/settings.py
+++ DetectiveLab_Site/DetectiveLab_Site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DetectiveLab_Site',
+    'Home_funcionalidades',
 ]
 
 MIDDLEWARE = [
---
+++ DetectiveLab_Site/Home_funcionalidades/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HomeFuncionalidadesConfig(AppConfig):
+    name = 'Home_funcionalidades'
