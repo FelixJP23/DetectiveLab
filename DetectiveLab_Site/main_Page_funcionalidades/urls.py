4527aef4755dacd2f056b06871e3b29b96d04842
FelixJP23 <felixsh230517@gmail.com>
2026-06-05 18:08:33 -0300

    Cards para visualizacão dos livros, feature de adicionar novos livros através do site.

--- DetectiveLab_Site/main_Page_funcionalidades/urls.py
+++ DetectiveLab_Site/main_Page_funcionalidades/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('biblioteca/', views.biblioteca,name='biblioteca'),
+    path('livro/<int:livro_id>/editar/', views.editar_livro, name='editar_livro'),
+    path('livro/criar/', views.criar_livro, name='criar_livro'),
 ]
